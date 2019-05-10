95493b57597649399e00a183944af8866ff5e42d
Patrick Raco <praco@onica.com>
2019-05-09 22:59:08 -0700

    add counting-website

---
+++ counting-website/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const redis = require('redis');
+const process = require('process');
+const app = express();
+const client = redis.createClient({
+  host: 'redis-server',
+  port: 6379
+});
+const port = 8082;
+client.set('visits', 0);
+
+app.get('/', (req, res) => {
+  process.exit(0);
+  client.get('visits', (err, visits) => {
+    client.set('visits', parseInt(visits) + 1);
+    res.send('Number of visits: [' + visits + ']');
+  });
+});
+
+app.listen(8082, () => {
+  console.log('Listening on port 8082');
+});
