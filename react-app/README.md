15a7be8c539aead55fdcdfac7a0d97466739030d
Patrick Raco <pataraco@gmail.com>
2019-05-19 18:07:23 -0700

    add react-app - WIP

---
+++ react-app/README.md
@@ -0,0 +1,19 @@
+# react app 
+
+## setup/use
+ 1. brew install node
+ 2. npm install -g create-react-app
+ 3. create-react-app frontend
+ 4. cd frontend
+ 4. npm test
+ 5. npm run build
+ 6. npm start (dev)
+ 7. docker build -f Dockerfile.dev
+ 8. rm -rf node_modules  # created by `npm run build`, not needed because
+                         # will be created with `RUN npm install` in Dockerfile
+ 9. docker build -f Dockerfile.dev
+10. docker run \
+       -v /var/local/app/node_modules \  # don't map volume, use container's
+       -v $(pwd):/var/local/app \        # map server volume to container's
+       -p 8083:3000 IMAGE
+
---
+++ react-app/frontend/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <i>Yo, yo, yo!</i> You can change "<code>src/App.js</code>" to customize this page. 
+	  Then save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
