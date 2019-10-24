226ef0cc12397f17ecc85c1cefe77bc224cd647d
Patrick Raco <pataraco@gmail.com>
2019-10-24 10:50:50 -0700

    add more tech info

--- fibo-web/client/src/App.js
+++ fibo-web/client/src/App.js
@@ -13,22 +13,28 @@ class App extends Component {
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
             <h1 className="App-title">
-	      Welcome to Fibonacci Calculator!
-	      <br/>
-	      <font size="3">(Powered by: React)</font>
-	    </h1>
+              Welcome to Fibonacci Calculator!
+              <br/>
+              <font size="3">(
+                Powered by: <Link to="https://reactjs.org/">React</Link>,
+                Created with: <Link to="https://www.terraform.io/">Terraform</Link> & <Link to="https://jenkins.io/">Jenkins</Link>,
+                Hosted on: <Link to="https://aws.amazon.com/">AWS</Link>,
+                Training/Project from: <Link to="https://www.udemy.com/course/docker-and-kubernetes-the-complete-guide/">Udemy</Link>,
+                Source files: <Link to="https://github.com/pataraco/docker/tree/master/fibo-web">GitHub</Link>
+              )</font>
+            </h1>
             | <Link to="/">Home</Link> |
             | <Link to="/otherpage">Another Page</Link> |
             <hr color="blue" size="3" width="50%"/>
           </header>
           <div>
-	    <br/>
-	    <br/>
+            <br/>
+            <br/>
             <Route exact path="/" component={Fib} />
             <Route path="/otherpage" component={OtherPage} />
           </div>
           <footer>
-	    <p><font size="2">(Version: 0.0.0)</font></p>
+            <p><font size="2">(Version: 0.0.1)</font></p>
           </footer>
         </div>
       </Router>
