1c78ddfcb299fa8491b16a43107c7bd15d2279fa
Patrick Raco <pataraco@gmail.com>
2019-10-25 11:14:46 -0700

    add route53 record, tf fmt and v0.12.x convert

--- fibo-web/terraform/main.tf
+++ fibo-web/terraform/main.tf
@@ -2,7 +2,7 @@
 #
 
 terraform {
-  backend "local" { }
+  backend "local" {}
   required_version = ">= 0.12.0"
 }
 
@@ -10,17 +10,25 @@ provider "aws" {
   region = "us-east-1"
 }
 
-data "aws_vpc" "default" {
-  default = true
+data "aws_route53_zone" "ijgbm" {
+  name = "ijustgottabe.me"
+}
+
+data "aws_elastic_beanstalk_hosted_zone" "ue1" {
+  region = "us-east-1"
+}
+
+data "aws_security_group" "default" {
+  vpc_id = data.aws_vpc.default.id
+  name   = "default"
 }
 
 data "aws_subnet_ids" "all" {
   vpc_id = data.aws_vpc.default.id
 }
 
-data "aws_security_group" "default" {
-  vpc_id = data.aws_vpc.default.id
-  name   = "default"
+data "aws_vpc" "default" {
+  default = true
 }
 
 resource "aws_security_group" "redis" {
@@ -29,11 +37,11 @@ resource "aws_security_group" "redis" {
   vpc_id      = data.aws_vpc.default.id
 
   egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
-    description     = "To Any"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "To Any"
   }
 
   tags = {
@@ -58,11 +66,11 @@ resource "aws_security_group" "rds" {
   vpc_id      = data.aws_vpc.default.id
 
   egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
-    description     = "To Any"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "To Any"
   }
 
   tags = {
@@ -79,4 +87,16 @@ resource "aws_security_group_rule" "rds" {
   protocol                 = "tcp"
   security_group_id        = aws_security_group.rds.id
   source_security_group_id = module.eb_env.security_group_id
+}
+
+resource "aws_route53_record" "fibo" {
+  zone_id = data.aws_route53_zone.ijgbm.zone_id
+  name    = "fibo.ijustgottabe.me"
+  type    = "A"
+
+  alias {
+    name                   = module.eb_env.endpoint
+    zone_id                = data.aws_elastic_beanstalk_hosted_zone.ue1.id
+    evaluate_target_health = true
+  }
 }
